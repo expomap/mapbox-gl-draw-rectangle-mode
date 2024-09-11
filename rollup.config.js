f16f658d2bfd036f866f34ba368119b889286079
Hugo Souza <hugo.souza0610@gmail.com>
2024-09-11 19:58:12 -0300

    ⬆️ build: Atualizei versão do Babel

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/index.js',
+        format: 'esm',
+        name: 'mapbox-gl-draw-rectangle-mode', 
+        exports: 'default'
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        babel({
+            exclude: 'node_modules/**',
+            babelHelpers: 'bundled',
+        }),
+    ],
+};
